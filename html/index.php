85fc9a42174ffd36e47fa3ade5b232e7fc619900
Lucas-Alarcon <lucas.alarcon@etu.univ-amu.fr>
2019-01-09 15:56:42 +0100

    lucas v2

---
+++ html/index.php
@@ -0,0 +1,139 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Evaluation Final</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+<header>
+    <div class="cacher_sprite">
+        <?php require "../img/sprite_symbol_pictos.svg" ?>
+    </div>
+
+    <div class="menu">
+        <img class="logo" alt="logo" src="../logos/EnergiesCollectives_rectangle_Logo_ajuste.svg">
+        <svg class="iconMenu">
+            <use xlink:href="#menu"></use>
+        </svg>
+        <!--<nav>
+                <ul>
+                    <li>En savoir plus</li>
+                    <li>Nous découvrir</li>
+                    <li>Nos Instalations</li>
+                    <li>Economisons</li>
+                    <li>En savoir plus</li>
+                    <li><input type="button" value="Souscire"></li>
+                </ul>
+            </nav>-->
+    </div>
+    <img class="photoHead" alt="photoHead" src="../Photos/20161202_101925.jpg">
+</header>
+
+<div class="main">
+
+    <div class="container">
+        <div class="slogan">
+            <img class="minilogo" alt="minilogo" src="../logos/LogoEnergiesCollectives_main.jpg">
+            L'énergie est notre affaire
+        </div>
+        <div class="pictos">
+            <div>
+                <svg class="icon">
+                    <use xlink:href="#picto_coop" width="50px" height="50px"></use>
+                </svg>
+            </div>
+            <div>
+                <svg class="icon">
+                    <use xlink:href="#picto_photovoltaique" width="50px" height="50px"></use>
+                </svg>
+            </div>
+            <div>
+                <svg class="icon">
+                    <use xlink:href="#picto_production_energie" width="50px" height="50px"></use>
+                </svg>
+            </div>
+            <div>
+                <svg class="icon">
+                    <use xlink:href="#picto_capital" width="50px" height="50px"></use>
+                </svg>
+            </div>
+        </div>
+
+        <div class="infos">
+            <div class="texte">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+            <div class="plus">
+                Comprendre notre modéle de production<br>
+                +
+            </div>
+            <div class="texte">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+
+            <div class="inscription">
+                Souscrire <img class="souscrire" alt="souscrire" src="../svg/schema_souscrire.svg">
+            </div>
+        </div>
+    </div>
+
+    <div class="article">
+        <div class="actu"><img class="photo" alt="photo" src="../Photos/IMG_20170121_122143.jpg">
+            </br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+        <div class="demarche">
+            <img class="photo" alt="photo" src="../Photos/IMG_4106.jpg">
+            </br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+        <div class="installation">
+            <img class="photo" alt="photo" src="../Photos/recadrage1_site.jpg">
+            </br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+        <div class="temoignage">
+            <img class="photo" alt="photo" src="../Photos/P1140365.jpg">
+            </br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+    </div>
+
+</div>
+
+<footer>
+    Nous suivre</br>
+    Nous contacter</br>
+    Rechercher</br>
+    Abonner-vous à la Newsletter</br>
+    <form>
+        <input type="text" id="email" name="email" required><input type="button" value="ok"></br>
+    </form>
+    <div class="partenaires">
+        Nos partenaires :
+        <img class="partenaire" alt="partenaire" src="../partenaires/enercoop.png">
+        <img class="partenaire" alt="partenaire" src="../partenaires/energie-partagee_logo.png">
+        <img class="partenaire" alt="partenaire" src="../partenaires/Initiative-nord-hautes-alpes.png">
+        <img class="partenaire" alt="partenaire" src="../partenaires/nef_logo.png">
+        <img class="partenaire" alt="partenaire" src="../partenaires/omnis_logo.png">
+        <img class="partenaire" alt="partenaire" src="../partenaires/paca_logo.png">
+        <img class="partenaire" alt="partenaire" src="../partenaires/parc_ecrins.png">
+        <img class="partenaire" alt="partenaire" src="../partenaires/syme05_logo.png">
+        <img class="partenaire" alt="partenaire" src="../partenaires/udess-05_logo-seul.png">
+    </div>
+</footer>
+
+</body>
+
+</html>
